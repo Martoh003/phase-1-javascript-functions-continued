8060e461596eb03b8d223826a658cf34a9f648a8
+ <+>
2023-07-14 09:49:40 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+// code your solution here return 
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Define the mondayWork function expression
+  const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  // Define the wrapAdjective function
+  function wrapAdjective(highlight = "*") {
+    return function (adjective) {
+      return `You are ${highlight}${adjective}${highlight}!`;
+    };
+  }
+  
+  module.exports = {
+    saturdayFun,
+    mondayWork,
+    wrapAdjective,
+  };
+  
